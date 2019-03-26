4065f18547ae0a31ebb854c54982d67c62e47360
kz159 <loh@kz159.ru>
2019-03-26 17:33:21 +0300

    its kind of working now

--- update.sh
+++ update.sh
@@ -1,36 +1,68 @@
 #!/bin/bash
-while getopts c:d:f: option
+while getopts b:d:c: option #ВОТ СЮДА ДОБАВИТЬ --no-backup
 do
 case "${option}"
 in
-c) CONFIG=${OPTARG};;
+b) BACKUP=${OPTARG};;
 d) DIR=${OPTARG};;
-f) CFG=$CFG;;
+c) CFG=${OPTARG};;
+#--no-backup) BACKUP_FLAG=false;;# НЕОБХОДИМО ЗАМУТИТЬ ФЛАГ БЕЗ БЕКАПА ДЛЯ ОПУХШИХ
 esac
 done
 
+if [ -z "$DIR" ] # Чекаем наличие папки
+then
+  echo "DIRECTORY WAS NOT SUPPLIED, EXITING!!"
+  exit
+fi
+
+if [ -z "$CFG" ] # Чекаем наличие конфигурационного файла
+then
+  echo "CONFIGURATION FILE WAS NOT SUPPLIED, EXITING!!"
+  exit
+fi
+
+if [ -z "$BACKUP" ] #чекаем место бэкапа
+then
+  echo "backup obsolete path was not specified, switching to /tmp!"
+  BACKUP=/tmp/1c_backup.tgz
+fi
+
 ONEC="/opt/1C/v8.3/x86_64/1cv8"
 
-DATE=$(date +"%d-%b-%Y")
-echo config = $CONFIG
-echo date = $DATE
+# Высвечиваем инфу шоб удобно было
+echo config = $CFG
 echo dir = $DIR
-echo format = $FORMAT
 echo 1c = $ONEC
+echo backup_name = $BACKUP
 
-echo -n stopping Apache...
-systemctl stop apach23231
+echo -n stopping Apache... # останавливаем апач
+systemctl stop apache232
 echo ok!
 
-echo -n making backup...
+echo -n making backup... # пилим бэкап
 sleep 3
-#tar -zcf "/mnt/backup/1cv8-$DATE.tgz" -C /opt/1C_bases/ Bases
+tar -zcf $BACKUP -P $DIR
 echo ok!
 
-for i in $DIR/*; do
+for i in $DIR/*; do #глядим во все папки баз
   echo -n "updating base $i..."
-  $ONEC DESIGNER /F $i /UpdateCFg $CONFIG /UpdateDBCfg
-  $ONEC ENTERPRISE /F $i
-  sleep 30
-  echo ok!
+  if [ -e "$i/1Cv8.1CD" ] #паралельно убеждаемся, что перед нами 1с8 база
+  then
+    echo -n found 1Cv8 file! Updating config!...
+    #$ONEC DESIGNER /F $i /UpdateCFg $CFG /UpdateDBCfg # фигачим апдейт
+    #$ONEC ENTERPRISE /F $i #Запускаем шоб руками доделать то, что руками надо
+    #ВОТ ТУТ БЫ ЕЩЕ XDOTOOL ПРИЦЕПИТЬ А
+    sleep 3
+    echo ok!
+  else
+    echo 'can`t find 1Cv8 file =C Doing nothing'
+    sleep 1
+  fi
 done
+
+echo -n Starting Apache...
+systemctl start apache2
+echo ok!
+
+echo 1c was updated!
